1a6ecc58d2c98277bc6d5c2bfd5c0e11073a824a
Abdelrahman Bahaaeldin <AbdelrahmanBonna@outlook.com>
2020-06-01 00:03:04 +0200

    Merge branch 'master' of D:\a.b.k\work\programing\projects\flutterapp\flutter_app\healthy_shield with conflicts.

--- lib/Screens/continueUserReg.dart
+++ lib/Screens/continueUserReg.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:healthyshield/Screens/login.dart';
 import 'package:healthyshield/Utilities/boolWidget.dart';
 import 'package:healthyshield/Utilities/constants.dart';
-import 'package:healthyshield/Utilities/dropmenu.dart';
 
 class ContinueUserReq extends StatefulWidget {
   static String id = "continueReq";
@@ -13,51 +11,10 @@ class ContinueUserReq extends StatefulWidget {
 }
 
 class _ContinueUserReqState extends State<ContinueUserReq> {
-  String carsNo,
-      job,
-      carModel,
-      mobileFees,
-      bloodType,
-      gender,
-      lostArm,
-      lostLeg,
-      income,
-      dependencies,
-      bD,
-      chronicDisease,
-      height,
-      weight;
-
-  Color male = Colors.white,
-      female = Colors.white,
-      yes0 = Colors.white,
-      no0 = Colors.white,
-      yes1 = Colors.white,
-      no1 = Colors.white,
-      yes2 = Colors.white,
-      no2 = Colors.white;
-
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(FontAwesomeIcons.backward),
-            title: Text('Back'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(FontAwesomeIcons.save),
-            title: Text('Submit'),
-          )
-        ],
-        onTap: (index) {
-          index == 1
-              ? Navigator.pushNamed(context, Login.id)
-              : Navigator.pop(context);
-        },
-      ),
       body: Container(
         height: mediaQuery.size.height,
         width: mediaQuery.size.width,
@@ -88,35 +45,15 @@ class _ContinueUserReqState extends State<ContinueUserReq> {
                   child: TextField(
                     style: Theme.of(context).textTheme.subtitle1,
                     decoration: kBirthDateTextFieldStyle,
-                    onChanged: (value) {
-                      setState(() {
-                        bD = value;
-                      });
-                    },
+                    onChanged: null,
                   ),
                 ),
                 Padding(
                   padding: kPaddingValue,
                   child: TextField(
                     style: Theme.of(context).textTheme.subtitle1,
-                    decoration: kHeightTextFieldStyle,
-                    onChanged: (value) {
-                      setState(() {
-                        height = value;
-                      });
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: kPaddingValue,
-                  child: TextField(
-                    style: Theme.of(context).textTheme.subtitle1,
-                    decoration: kWeightTextFieldStyle,
-                    onChanged: (value) {
-                      setState(() {
-                        weight = value;
-                      });
-                    },
+                    decoration: kJobTextFieldStyle,
+                    onChanged: null,
                   ),
                 ),
                 Padding(
@@ -124,11 +61,7 @@ class _ContinueUserReqState extends State<ContinueUserReq> {
                   child: TextField(
                     style: Theme.of(context).textTheme.subtitle1,
                     decoration: kMIncomeTextFieldStyle,
-                    onChanged: (value) {
-                      setState(() {
-                        income = value;
-                      });
-                    },
+                    onChanged: null,
                   ),
                 ),
                 Padding(
@@ -136,167 +69,71 @@ class _ContinueUserReqState extends State<ContinueUserReq> {
                   child: TextField(
                     style: Theme.of(context).textTheme.subtitle1,
                     decoration: kNoOfDepTextFieldStyle,
-                    onChanged: (value) {
-                      setState(() {
-                        dependencies = value;
-                      });
-                    },
+                    onChanged: null,
                   ),
                 ),
                 BoolWidget(
-                  firstChoiceColor: male,
-                  secondChoiceColor: female,
-                  title: 'Choose your Gender',
-                  firstIcon: FontAwesomeIcons.male,
-                  secondIcon: FontAwesomeIcons.female,
-                  firstText: 'Male',
-                  secondText: 'Female',
+                  firstChoiceColor: Colors.white,
+                  secondChoiceColor: Colors.white,
+                  title: 'Gender',
+                  firstIcon: FontAwesomeIcons.female,
+                  secondIcon: FontAwesomeIcons.male,
+                  firstText: 'Female',
+                  secondText: 'Male',
                   firstChoiceDo: () {
-                    setState(() {
-                      gender = 'male';
-                      male = Colors.amber;
-                      female = Colors.white;
-                    });
+                    print("1");
                   },
                   secondChoiceDo: () {
-                    setState(() {
-                      gender = 'female';
-                      female = Colors.greenAccent;
-                      male = Colors.white;
-                    });
+                    print("2");
                   },
                 ),
                 BoolWidget(
-                  firstChoiceColor: yes0,
-                  secondChoiceColor: no0,
-                  title: 'Do you have Cut Of Legs',
+                  firstChoiceColor: Colors.white,
+                  secondChoiceColor: Colors.white,
+                  title: 'Cut Of Legs',
                   firstIcon: FontAwesomeIcons.check,
                   secondIcon: FontAwesomeIcons.times,
                   firstText: 'Yes',
                   secondText: 'No',
                   firstChoiceDo: () {
-                    setState(() {
-                      lostLeg = 'yes';
-                      yes0 = Colors.greenAccent;
-                      no0 = Colors.white;
-                    });
+                    print("1");
                   },
                   secondChoiceDo: () {
-                    setState(() {
-                      lostLeg = 'no';
-                      yes0 = Colors.white;
-                      no0 = Colors.redAccent;
-                    });
+                    print("2");
                   },
                 ),
                 BoolWidget(
-                  firstChoiceColor: yes1,
-                  secondChoiceColor: no1,
-                  title: 'Do you have Cut Of arms',
+                  firstChoiceColor: Colors.white,
+                  secondChoiceColor: Colors.white,
+                  title: 'Cut Of arms',
                   firstIcon: FontAwesomeIcons.check,
                   secondIcon: FontAwesomeIcons.times,
                   firstText: 'Yes',
                   secondText: 'No',
                   firstChoiceDo: () {
-                    setState(() {
-                      lostArm = 'yes';
-                      yes1 = Colors.greenAccent;
-                      no1 = Colors.white;
-                    });
+                    print("1");
                   },
                   secondChoiceDo: () {
-                    setState(() {
-                      lostArm = 'no';
-                      yes1 = Colors.white;
-                      no1 = Colors.redAccent;
-                    });
+                    print("2");
                   },
                 ),
                 BoolWidget(
-                  firstChoiceColor: yes2,
-                  secondChoiceColor: no2,
-                  title: 'Do you have Chronic Disease?',
+                  firstChoiceColor: Colors.white,
+                  secondChoiceColor: Colors.white,
+                  title: 'Chronic Disease',
                   firstIcon: FontAwesomeIcons.check,
                   secondIcon: FontAwesomeIcons.times,
                   firstText: 'Yes',
                   secondText: 'No',
                   firstChoiceDo: () {
-                    setState(() {
-                      chronicDisease = 'yes';
-                      yes2 = Colors.greenAccent;
-                      no2 = Colors.white;
-                    });
+                    print("1");
                   },
                   secondChoiceDo: () {
-                    setState(() {
-                      chronicDisease = 'no';
-                      yes2 = Colors.white;
-                      no2 = Colors.redAccent;
-                    });
+                    print("2");
                   },
-                ),
-                Padding(
-                  padding: kPaddingValue,
-                  child: DropDownMenuCustom(
-                    title: 'No of owned cars:',
-                    itemsStrings: ['1', '2', '<=3'],
-                    hint: 'No of cars',
-                    f: (value) {
-                      setState(() {
-                        carsNo = value;
-                      });
-                    },
-                    value: carsNo,
-                  ),
-                ),
-                Padding(
-                  padding: kPaddingValue,
-                  child: DropDownMenuCustom(
-                    title: 'Cars Models:',
-                    hint: 'Car Model',
-                    itemsStrings: ['Model>2013', 'Model<2013'],
-                    f: (value) {
-                      setState(() {
-                        carModel = value;
-                      });
-                    },
-                    value: carModel,
-                  ),
-                ),
-                Padding(
-                  padding: kPaddingValue,
-                  child: DropDownMenuCustom(
-                    title: "annually mobile fee's",
-                    hint: 'Mobile fees',
-                    itemsStrings: ['fees>800EGP', 'fees<800EGP'],
-                    f: (value) {
-                      setState(() {
-                        mobileFees = value;
-                      });
-                    },
-                    value: mobileFees,
-                  ),
-                ),
-                Padding(
-                  padding: kPaddingValue,
-                  child: DropDownMenuCustom(
-                    title: 'Your job:',
-                    itemsStrings: [
-                      'Student',
-                      'Jobless',
-                      'Private work',
-                      'Government'
-                    ],
-                    f: (value) {
-                      setState(() {
-                        job = value;
-                      });
-                    },
-                    hint: 'Job type',
-                    value: job,
-                  ),
-                ),
+                )
               ],
+              //Barcode screen
             ),
           ),
         ),
