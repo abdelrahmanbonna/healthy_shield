db64f19a318301e720d54d664c0180ed344ab802
Abdelrahman Bahaaeldin <AbdelrahmanBonna@outlook.com>
2020-06-04 21:30:16 +0200

    medicalplaces class
    Sarah Hatem
    
    Signed-off-by: Abdelrahman Bahaaeldin <AbdelrahmanBonna@outlook.com>

--- lib/models/medicalPlace.dart
+++ lib/models/medicalPlace.dart
@@ -1,3 +1,60 @@
 class MedicalPlace {
-  String _ID, _Name, _Phone, _Address; // ignore: non_constant_identifier_names
+  String _ID, _Name, _Phone, _Address, _Email;
+  double _VisitFees;
+  void setAllDataUsage(email, name, phone, address, visitFee) {
+    _Email = email;
+    _Name = name;
+    _Phone = phone;
+    _Address = address;
+    _VisitFees = visitFee;
+  }
+
+  void setID(id) {
+    _ID = id;
+  }
+
+  void setEmail(email) {
+    _Email = email;
+  }
+
+  void setName(name) {
+    _Name = name;
+  }
+
+  void setPhone(phone) {
+    _Phone = phone;
+  }
+
+  void setAddress(address) {
+    _Address = address;
+  }
+
+  void setVisitFees(visitFee) {
+    _VisitFees = visitFee;
+  }
+
+  getID() {
+    return _ID;
+  }
+
+  getEmail() {
+    return _Email;
+  }
+
+  getName() {
+    return _Name;
+  }
+
+  getPhone() {
+    return _Phone;
+  }
+
+  getAddress() {
+    return _Address;
+  }
+
+  getVisitFees() {
+    return _VisitFees;
+  }
+// ignore: non_constant_identifier_names
 }
