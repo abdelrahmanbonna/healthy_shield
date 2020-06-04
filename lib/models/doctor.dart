81ce5bc70218f4a604da0e3ed4473e7af862cc0a
Abdelrahman Bahaaeldin <AbdelrahmanBonna@outlook.com>
2020-06-04 20:57:59 +0200

    doctor's class
    Sarah Hatem
    
    Signed-off-by: Abdelrahman Bahaaeldin <AbdelrahmanBonna@outlook.com>

--- lib/models/doctor.dart
+++ lib/models/doctor.dart
@@ -1,1 +1,64 @@
-class Doctor {}
+class Doctor {
+  String _ID;
+  String _Email;
+  String _Mobile;
+  String _FirstName;
+  String _LastName;
+  String _Specialty;
+
+  void setAllDataUsage(fname, lname, mobile, specialty, email) {
+    _FirstName = fname;
+    _LastName = lname;
+    _Email = email;
+    _Specialty = specialty;
+    _Mobile = mobile;
+  }
+
+  void setID(id) {
+    _ID = id;
+  }
+
+  void setEmail(email) {
+    _Email = email;
+  }
+
+  void setFirstName(name) {
+    _FirstName = name;
+  }
+
+  void setLasName(name) {
+    _LastName = name;
+  }
+
+  void setMobile(mobile) {
+    _Mobile = mobile;
+  }
+
+  void setSpecialty(specialty) {
+    _Specialty = specialty;
+  }
+
+  getID() {
+    return _ID;
+  }
+
+  getEmail() {
+    return _Email;
+  }
+
+  getFirstName() {
+    return _FirstName;
+  }
+
+  getLasName() {
+    return _LastName;
+  }
+
+  getMobile() {
+    return _Mobile;
+  }
+
+  getSpecialty() {
+    return _Specialty;
+  }
+}
